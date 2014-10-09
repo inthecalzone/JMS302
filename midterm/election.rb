6e44ae88bb5640359948f33af27c7f193200285c
Jane Hammond <Jane>
2014-10-09 10:41:06 -0400

    Election problem changes (still wrong)

--- midterm/election.rb
+++ midterm/election.rb
@@ -1,37 +1,32 @@
-def nontraditional_marriage(last_name, partner_last_name)
-     last_name += "-" + partner_last_name
-end
-
-puts "Enter your last name:"
-last_name = gets.chomp
-puts "Enter your partner's last name:"
-partner_last_name =gets.chomp
-last_name = nontraditional_marriage(last_name, partner_last_name)
-puts last_name
-
 results = []
 
-#def get_results(candidate_name)
+def get_results(candidate_name)
     puts "What is the candidate's name?"
     candidate_name = gets.chomp
     candidate_name
 end
 
-#get_results("Michelle Nunn")
+get_results "Michelle Nunn"
 
-#puts "What are his/her results? Type 'done' when you're done."
+puts "What are his/her results? Type 'done' when you're done."
     while true
         result = gets.chomp
     if (result.downcase == 'done')
         break
     else 
-        results.push(result.to_i)
+        results.push result.to_i
     end
 end
 
-total_votes = 0
-results.each do |result|
-  voting_percentage = total_votes + result
+def calculate_results(results)
+    total_votes = 0
+    results.each do |result|
+        total_votes += result.to_i
+        end
+    results.each do |result|
+        total_percentage = (result/total_votes) * 100
+        end
 end
+        
+puts "#{candidate_name} is winning with #{total_percentage}% of the votes."
 
-#puts "#{candidate_name} is winning with #{voting_percentage}% of the vote"
