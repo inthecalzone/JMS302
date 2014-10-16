6e040f27582c85b3ccd4571a65721624f55efae1
Jane Hammond <Jane>
2014-10-16 13:22:45 -0400

    Working on class problems, new midterm problem

---
+++ midterm/election2.rb
@@ -0,0 +1,22 @@
+def get_results(candidate)
+    puts "What votes did #{ candidate } get in each precinct? Enter 'done' when done."
+    total = 0
+    while true
+        votes = gets.chomp
+    if votes.downcase == 'done'
+        break
+    end
+        total += votes.to_i
+    end
+    total
+end
+
+nunn_total = get_results "Michelle Nunn" 
+perdue_total = get_results "David Perdue"
+
+
+nunn_percentage = (nunn_total.to_f / (nunn_total.to_f + perdue_total.to_f)) * 100
+perdue_percentage = (perdue_total.to_f / (nunn_total.to_f + perdue_total.to_f)) * 100
+
+puts "Michelle Nunn had #{ nunn_percentage }% of the votes."
+puts "David Perdue had #{ perdue_percentage }% of the votes."
